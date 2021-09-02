b77e0abcc752b8a922e88b99d68bdf3c8dacfa66
Alisher-Usmonov <alisherufullstack@gmail.com>
2021-09-02 16:43:32 +0500

    getImage chnaged

--- src/Adapters/GetImage.js
+++ src/Adapters/GetImage.js
@@ -1,5 +1,6 @@
 export  const GetImage = async (query, type, setData, other) => {
-    let fetchData = await fetch(`https://pixabay.com/api/?key=23049458-4447dba852075312d87880013&q=${query}&image_type=${type}&pretty=true${other}`);
+    let q = query.trim().split(" ").length > 1 ? query.trim().split(" ").join("+") : query;
+    let fetchData = await fetch(`https://pixabay.com/api/?key=23049458-4447dba852075312d87880013&q=${q}&image_type=${type}&pretty=true${other}`);
     let res = await fetchData.json();
     setData(res);
 };
